6b95fd37881fbbd8efedf78f4086957c1f2b3630
asdera <poiuytandrew@gmail.com>
2015-05-07 17:50:46 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,21 @@
-$(document).ready(function () { 
-   $('img').click(function () {
-          $(this).effect("bounce", { times:5 }, 1000);
-    });
-    $(document).keydown(function(key) {
-        switch(parseInt(key.which,10)) {
-            case 65:
-                $('#guy').animate({left: "-=50px"}, "fast");
-                break;
-            case 68:
-                $('#guy').animate({left: "+=50px"}, "fast");
-                break;
-            default:
-                break;
-        }
-    });
-});
-
+$(this).effect("bounce", { times:5 }, 1000);
+     });
++        switch(parseInt(key.which,10)) {
++            case 65:
++                $('#guy').animate({left: "-=50px"}, 'fast');
++                break;
++            case 83:
++                $('#guy').animate({top: "+=10px"}, 'fast');
++                break;
++            case 87:
++                $('#guy').animate({top: "-=10px"}, 'fast');
++                break;
++            case 68:
++                $('#guy').animate({left: "+=50px"}, 'fast');
++                break;
++            default:
++                break;
++        }
++    });
+ });
+ 

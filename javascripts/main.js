14563a50c343a62d8d970c93609a72ea23226444
asdera <poiuytandrew@gmail.com>
2015-05-07 17:54:04 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,24 @@
-$(this).effect("bounce", { times:5 }, 1000);
-     });
-+        switch(parseInt(key.which,10)) {
-+            case 65:
-+                $('#guy').animate({left: "-=50px"}, 'fast');
-+                break;
-+            case 83:
-+                $('#guy').animate({top: "+=10px"}, 'fast');
-+                break;
-+            case 87:
-+                $('#guy').animate({top: "-=10px"}, 'fast');
-+                break;
-+            case 68:
-+                $('#guy').animate({left: "+=50px"}, 'fast');
-+                break;
-+            default:
-+                break;
-+        }
-+    });
- });
- 
+
+$(document).ready(function () { 
+   $('#img').click(function () {
+          $(this).effect("bounce", { times:5 }, 1000);
+    });
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+            case 65:
+                $('#guy').animate({left: "-=10px"}, 'fast');
+                break;
+            case 83:
+                $('#guy').animate({top: "+=10px"}, 'fast');
+                break;
+            case 87:
+                $('#guy').animate({top: "-=10px"}, 'fast');
+                break;
+            case 68:
+                $('#guy').animate({left: "+=10px"}, 'fast');
+                break;
+            default:
+                break;
+        }
+    });
+});

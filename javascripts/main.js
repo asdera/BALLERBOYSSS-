43c3fbfb589c9206b10c287c2ff8182491323fb7
asdera <poiuytandrew@gmail.com>
2015-05-07 17:35:04 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,27 +3,22 @@ $(document).ready(function () {
           $(this).effect("bounce", { times:5 }, 1000);
     });
     $(document).keydown(function(key) {
-switch(parseInt(key.which,10)) {
-// Left arrow key pressed
-case 37:
-$('#guy').animate({left: "-=10px"}, 'fast');
-break;
-// Up Arrow Pressed
-case 38:
-$('#guy').animate({up: "-=10px"}, 'fast');
-// Put our code here
-break;
-// Right Arrow Pressed
-case 39:
-$('#guy').animate({right: "+=10px"}, 'fast');
-// Put our code here
-break;
-// Down Arrow Pressed
-case 40:
-// Put our code here
-$('#guy').animate({down: "+=10px"}, 'fast');
-break;
-}
-});
+        switch(parseInt(key.which,10)) {
+            case 65:
+                $('#guy').animate({left: "-=10px"}, 'fast');
+                break;
+            case 83:
+                $('#guy').animate({top: "+=10px"}, 'fast');
+                break;
+            case 87:
+                $('#guy').animate({top: "-=10px"}, 'fast');
+                break;
+            case 68:
+                $('#guy').animate({left: "+=10px"}, 'fast');
+                break;
+            default:
+                break;
+        }
+    });
 });
 

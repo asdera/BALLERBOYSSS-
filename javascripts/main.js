c30b2cc73ba382def4caa2357768ed47c795a27a
asdera <poiuytandrew@gmail.com>
2015-05-07 17:07:27 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
           $(this).effect("bounce", { times:5 }, 1000);
     });
 });
+var ball = Physics.body('circle', {
+    x: 50, // x-coordinate
+    y: 30, // y-coordinate
+    vx: 0.2, // velocity in x-direction
+    vy: 0.01, // velocity in y-direction
+    radius: 20
+});
+// add the circle to the world
+world.add( ball );
